9029d1fe2a5b9d3c68c6be5f4e87283b0f710ccc
Abhishek_jain <abhisekj875@gmail.com>
2025-12-06 19:55:37 +0530

    make Chat Ai frontend

--- Frontend/src/components/chatAi.jsx
+++ Frontend/src/components/chatAi.jsx
@@ -3,16 +3,75 @@ import { useForm } from "react-hook-form";
 import axiosClient from '../utils/axios'
 import { Send } from "lucide-react";
 
-function chatAi({problem}){
-    const [msg , setmsg] = useState([
+export default function ChatAi({problem}) {
+    const [message , setmessage] = useState([
         {role: 'model', content : 'hi, how are you' },
         {role: 'user', content: 'hi, I am fine'}
     ])
 
-    const {register , handleSbumit, reset, formState: {error} } = useForm;
+    const {register , handleSbumit, reset, formState: {errors} } = useForm;
     const Endmessageref = useRef(null)
 
     useEffect(()=>{
         Endmessageref.current?.scrollIntoview({behavior : 'smooth'})
-    },[msg])
+    },[message])
+
+    const onSubmit = async (data) => {
+        setmessage( (prev) => [...prev ,{role: 'user', content: data.message}])
+        reset();
+
+        try{
+            const response = await axiosClient.post('/chat/ai', {
+                message: data.message
+            });
+
+            setmessage([...prev, {
+                role: 'model',
+                content: response.data.message || response.data.content
+            }])
+        }
+        catch(error){
+            console.error('error occured');
+            setmessage([...prev, {
+                role:'model',
+                content: 'sorry , i encounterd an error'
+            }])
+        }
+    }
+
+    return(
+        <div className="flex flex-col h-screen max-h-[80vh] min-h-[500px]">
+          <div className="flex-1 overflow-y-auto p-4 space-y-4">
+            {message.map((msg, index) => (
+                <div key={index}
+                className={`chat ${msg.role === 'user' ? "chat-end" : "chat-start"}`}
+                >
+                  <div className="chat-bubble text-base-content bg-base-200">
+                    {msg.content}
+                  </div>
+                </div>
+            ))}
+            <div ref={Endmessageref}/>
+
+          </div>
+
+            <form onSubmit={handleSbumit(onsubmit)}
+            className="sticky bottom-0 p-4 bg-base-100 border-t"
+            >
+               <div className="flex items-center">
+
+                  <input 
+                  placeholder="Ask me anything"
+                  className="input input-bordered flex-1"
+                  {...register("message", {required: true, minLength: 2})}
+                />
+                <button type="submit" className="btn btn-ghost ml-2" disabled = {errors.message} >
+                   <Send size={20}></Send>
+                </button>
+            
+               </div>
+            </form>
+
+        </div>
+    );
 }
